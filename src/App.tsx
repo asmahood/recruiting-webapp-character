e889332268923442b2c58f958de33ecee9856eda
Adam Mahood <mahood.adam@gmail.com>
2024-11-14 15:23:44 -0500

    feat: add controls for attributes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import "./App.css";
+import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from "./consts";
+import { Attribute } from "./components/Attribute";
 
 function App() {
-  const [num, setNum] = useState<number>(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Coding Exercise</h1>
+        <h1>Character Creator</h1>
       </header>
       <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
+        <h2>Attributes</h2>
+        <section className="Attribute-list">
+          {ATTRIBUTE_LIST.map((attribute) => (
+            <Attribute name={attribute} />
+          ))}
+        </section>
       </section>
     </div>
   );
